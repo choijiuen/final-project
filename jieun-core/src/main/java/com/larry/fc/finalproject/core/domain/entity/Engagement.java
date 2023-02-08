c4903fde79451400023660fb141dbded0e2ea0fa
choijiuen <choijiuen@gmail.com>
2023-02-08 16:29:55 +0900

    domain plan -2

---
+++ jieun-core/src/main/java/com/larry/fc/finalproject/core/domain/entity/Engagement.java
@@ -0,0 +1,30 @@
+package com.larry.fc.finalproject.core.domain.entity;
+
+import com.larry.fc.finalproject.core.domain.Event;
+import com.larry.fc.finalproject.core.domain.RequestStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "engagements")
+public class Engagement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JoinColumn(name = "schedule_id")
+    @ManyToOne
+    private Schedule schedule;
+
+    @JoinColumn(name = "attendee_id")
+    @ManyToOne
+    private User attendee;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private RequestStatus requestStatus;
+
+}
---
+++ jieun-core/src/main/java/com/larry/fc/finalproject/core/domain/entity/Schedule.java
@@ -0,0 +1,81 @@
+package com.larry.fc.finalproject.core.domain.entity;
+
+import com.larry.fc.finalproject.core.domain.Event;
+import com.larry.fc.finalproject.core.domain.Notification;
+import com.larry.fc.finalproject.core.domain.ScheduleType;
+import com.larry.fc.finalproject.core.domain.Task;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "schedules")
+@Entity
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String title;
+    private String description;
+    @JoinColumn(name="writer_id")
+    @ManyToOne
+    private User writer;
+
+    @Enumerated(EnumType.STRING)
+    private ScheduleType scheduleType;
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public static Schedule event(String title, String description, LocalDateTime startAt, LocalDateTime endAt, User writer){
+        return Schedule.builder()
+                .title(title)
+                .description(description)
+                .startAt(startAt)
+                .endAt(endAt)
+                .writer(writer)
+                .scheduleType(ScheduleType.EVENT)
+                .build();
+
+        //이런 인자들을 가지고 이벤트를 만들어라고 했을 때 , 이벤트가 스케줄 entity 이기 때문에
+        //.scheduleType(ScheduleType.EVENT) 이렇게 이벤트를 지정해줘서 스케줄 entity를 생성해낸다
+    }
+
+    public static Schedule task(String title, String description, LocalDateTime taskAt, User writer){
+        return Schedule.builder()
+                .title(title)
+                .description(description)
+                .startAt(taskAt)
+                .writer(writer)
+                .scheduleType(ScheduleType.TASK)
+                .build();
+    }
+
+    public static Schedule notification(String title, LocalDateTime notifyAt, User writer){
+        return Schedule.builder()
+                .title(title)
+                .startAt(notifyAt)
+                .writer(writer)
+                .scheduleType(ScheduleType.TASK)
+                .build();
+    }
+
+    public Task toTask(){
+        return new Task(this);
+    }
+
+    public Event toEvent(){
+        return new Event(this);
+    }
+
+    public Notification toNotification(){
+        return new Notification(this);
+    }
+}
