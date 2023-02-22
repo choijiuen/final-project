e9f32f63182d8181448a515f7143581d25143794
choijiuen <choijiuen@gmail.com>
2023-02-22 20:51:16 +0900

    inquiry logic modified

--- jieun-api/src/main/java/com/larry/fc/finalproject/api/service/ScheduleQueryService.java
+++ jieun-api/src/main/java/com/larry/fc/finalproject/api/service/ScheduleQueryService.java
@@ -3,8 +3,6 @@ package com.larry.fc.finalproject.api.service;
 import com.larry.fc.finalproject.api.dto.AuthUser;
 import com.larry.fc.finalproject.api.dto.ScheduleDto;
 import com.larry.fc.finalproject.api.util.DtoConverter;
-import com.larry.fc.finalproject.core.domain.entity.Engagement;
-import com.larry.fc.finalproject.core.domain.entity.Schedule;
 import com.larry.fc.finalproject.core.domain.entity.repository.EngagementRepository;
 import com.larry.fc.finalproject.core.domain.entity.repository.ScheduleRepository;
 import com.larry.fc.finalproject.core.util.Period;
@@ -26,43 +24,29 @@ public class ScheduleQueryService {
     private final EngagementRepository engagementRepository;
 
     public List<ScheduleDto> getScheduleByDay(AuthUser authUser, LocalDate date) {
+        return getScheduleByPeriod(authUser, Period.of(date, date));
+    }
+
+    public List<ScheduleDto> getScheduleByWeek(AuthUser authUser, LocalDate startOfWeek) {
+        return getScheduleByPeriod(authUser, Period.of(startOfWeek, startOfWeek.plusDays(6)));
+    }
+
+    public List<ScheduleDto> getScheduleByMonth(AuthUser authUser, YearMonth yearMonth) {
+        return getScheduleByPeriod(authUser, Period.of(yearMonth.atDay(1), yearMonth.atEndOfMonth()));
+    }
+    
+    private List<ScheduleDto> getScheduleByPeriod(AuthUser authUser, Period period) {
         return Stream.concat(
                         scheduleRepository.findAllByWriter_Id(authUser.getId())
                                 .stream()
-                                .filter(schedule -> schedule.isOverlapped(date)) //특정 date와 겹치는 것만 필터링을 한다
+                                .filter(schedule -> schedule.isOverlapped(period)) //특정 period와 겹치는 것만 필터링을 한다
                                 .map(schedule -> DtoConverter.fromSchedule(schedule)),
                         engagementRepository.findAllByAttendee_Id(authUser.getId())
                                 .stream()
-                                .filter(engagement -> engagement.isOverlapped(date))
+                                .filter(engagement -> engagement.isOverlapped(period))
                                 .map(engagement -> DtoConverter.fromSchedule(engagement.getSchedule()))) //위 두 개 메서드 합칠 수 있다. concat
                 .collect(Collectors.toList());
     }
 
-    public List<ScheduleDto> getScheduleByWeek(AuthUser authUser, LocalDate startOfWeek) {
-        final Period period = Period.of(startOfWeek, startOfWeek.plusDays(6));
-        return Stream.concat(
-                        scheduleRepository.findAllByWriter_Id(authUser.getId())
-                                .stream()
-                                .filter(schedule -> schedule.isOverlapped(period))
-                                .map(schedule -> DtoConverter.fromSchedule(schedule)),
-                        engagementRepository.findAllByAttendee_Id(authUser.getId())
-                                .stream()
-                                .filter(engagement -> engagement.isOverlapped(period))
-                                .map(engagement -> DtoConverter.fromSchedule(engagement.getSchedule())))
-                .collect(Collectors.toList());
-    }
 
-    public List<ScheduleDto> getScheduleByMonth(AuthUser authUser, YearMonth yearMonth) {
-        final Period period = Period.of(yearMonth.atDay(1), yearMonth.atEndOfMonth());
-        return Stream.concat(
-                        scheduleRepository.findAllByWriter_Id(authUser.getId())
-                                .stream()
-                                .filter(schedule -> schedule.isOverlapped(period))
-                                .map(schedule -> DtoConverter.fromSchedule(schedule)),
-                        engagementRepository.findAllByAttendee_Id(authUser.getId())
-                                .stream()
-                                .filter(engagement -> engagement.isOverlapped(period))
-                                .map(engagement -> DtoConverter.fromSchedule(engagement.getSchedule())))
-                .collect(Collectors.toList());
-    }
 }
